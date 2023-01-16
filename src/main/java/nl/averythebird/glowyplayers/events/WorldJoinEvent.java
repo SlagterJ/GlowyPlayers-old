b95420bf3460b3587e38e9ac6b1431cb5e617f7a
Jordy Slagter <slagterj2001@gmail.com>
2023-01-16 12:09:24 +0100

    Updated to 1.19.3

--- src/main/java/nl/averythebird/glowyplayers/events/WorldJoinEvent.java
+++ src/main/java/nl/averythebird/glowyplayers/events/WorldJoinEvent.java
@@ -18,7 +18,7 @@ public class WorldJoinEvent {
   private static final Logger logger = getLogger("event");
 
   public static void onSpawn(ServerWorld world, Entity entity) {
-    if (world.isClient) return;
+    if (world.isClient()) return;
     if (!(entity instanceof final PlayerEntity player)) return;
 
     logger.debug("Applying glowing effect to player " + player.getDisplayName());
